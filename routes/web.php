43b2f46eeb6894a82598c372c821d6b5305e454d
beovan <truongvangiap04@gmail.com>
2023-09-16 10:22:24 +0700

    ADD route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\MenuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,24 @@ use \App\Http\Controllers\Admin\Users\LoginController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+//chuyển đăng nhặp
+Route::get('admin/users/login', [LoginController::class, 'index'])->name('login');
+//chuyển sang trang admin
+Route::post('admin/users/login/store', [LoginController::class, 'store']);
 
-Route::get(uri: 'admin/users/login', action: [LoginController::class, 'index']);
-Route::post(uri: 'admin/users/login/store', action: [LoginController::class, 'store']);
+//group lại
+Route::middleware(['auth'])->group(function () {
+//tiền tố định tuyến: Thuộc tính prefix có thể sử dụng để thêm tiền tố cho mỗi định tuyến trong một nhóm với một URI.
+// Ví dụ, bạn có thể muốn tất cả các tiền tố trong nhóm là admin:
+    Route::prefix('admin')->group(function () {
+        Route::get('/', [MainController::class, 'index'])->name('admin');
+        Route::get('main', [MainController::class, 'index']);
+
+
+        #MENU
+        Route::prefix('menus')->group(function () {
+            Route::get('add',[MenuController::class,'create']);
+        });
+    });
+});
+
