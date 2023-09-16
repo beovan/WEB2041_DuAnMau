992ac127d2350963aa6ad370de1e79dfa52fb88f
beovan <truongvangiap04@gmail.com>
2023-09-16 10:19:45 +0700

    ADD aleart (thông báo lỗi)

--- resources/views/admin/alert.blade.php
+++ resources/views/admin/alert.blade.php
@@ -7,3 +7,18 @@
         </ul>
     </div>
 @endif
+
+
+
+@if(Session::has('error'))
+    <div class="alert alert-danger">
+        {{Session::get('error')}}
+    </div>
+@endif
+
+
+@if(Session::has('success'))
+    <div class="alert alert-success">
+        {{Session::get('success')}}
+    </div>
+@endif
