fdd5681154d7ee87e77cd5e5140f77d7cec5683c
beovan <truongvangiap04@gmail.com>
2023-09-18 20:44:00 +0700

    Add ckeditor and "Them danh muc"

--- resources/views/admin/menu/add.blade.php
+++ resources/views/admin/menu/add.blade.php
@@ -1,42 +1,62 @@
 @extends('admin.main')
 
+@section('head')
+    <script src="https://cdn.ckeditor.com/4.22.1/full/ckeditor.js"></script>
+{{--<script src="/ckeditor/ckeditor.js"></script>--}}
+@endsection
+
 @section('content')
     <form action="" method="POST">
         <div class="card-body">
+
             <div class="form-group">
-                <label for="exampleInputEmail1">Tên Danh Mục</label>
-                <input type="text" name="menu" class="form-control" id="menu" placeholder="Enter name">
+                <label for="menu">Tên Danh Mục</label>
+                <input type="text" name="name" class="form-control"  placeholder="Nhập tên danh mục">
             </div>
 
             <div class="form-group">
-                <label for="exampleInputEmail1">Danh Mục</label>
+                <label>Danh Mục</label>
                 <select class="form-control" name="parent_id">
-                    <option value="0">Danh mục cha</option>
+                    <option value="0"> Danh Mục Cha </option>
+
                 </select>
             </div>
 
+            <div class="form-group">
+                <label>Mô Tả </label>
+                <textarea name="description" class="form-control"></textarea>
+            </div>
 
             <div class="form-group">
-                <label for="exampleInputFile">File input</label>
-                <div class="input-group">
-                    <div class="custom-file">
-                        <input type="file" class="custom-file-input" id="exampleInputFile">
-                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                    </div>
-                    <div class="input-group-append">
-                        <span class="input-group-text">Upload</span>
-                    </div>
+                <label>Mô Tả Chi Tiết</label>
+                <textarea name="content" id="content" class="form-control"></textarea>
+            </div>
+
+
+            <div class="form-group">
+                <label>Kích Hoạt</label>
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" value="1" type="radio" id="active" name="active" checked="">
+                    <label for="active" class="custom-control-label">Có</label>
+                </div>
+                <div class="custom-control custom-radio">
+                    <input class="custom-control-input" value="0" type="radio" id="no_active" name="active" >
+                    <label for="no_active" class="custom-control-label">Không</label>
                 </div>
             </div>
-            <div class="form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
-            </div>
+
         </div>
-        <!-- /.card-body -->
 
         <div class="card-footer">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Tạo Danh Mục</button>
         </div>
+        @csrf
     </form>
+@endsection
+
+@section('footer')
+    <script>
+        CKEDITOR.replace( 'content' );
+    </script>
+
 @endsection
