baffaaf2b663553bf152f7293efa251a6aa9539a
beovan <truongvangiap04@gmail.com>
2023-09-20 10:55:07 +0700

    add  update , destroy

--- app/Http/Services/Menu/MenuService.php
+++ app/Http/Services/Menu/MenuService.php
@@ -4,7 +4,7 @@ namespace App\Http\Services\Menu;
 
 use App\Models\Menu;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Str;
+
 
 class MenuService
 {
@@ -38,5 +38,53 @@ class MenuService
         }
 
         return true;
+    }
+
+    public function  update($request, $menu) : bool
+    {
+        if ($request->input('parent_id') != $menu->id){
+            $menu->parent_id = (string) $request->input('parent_id');
+        }
+
+
+        $menu->name = (string) $request->input('name');
+        $menu->description = (string) $request->input('description');
+        $menu->content = (string) $request->input('content');
+        $menu->active = (string) $request->input('active');
+        $menu->save();
+
+        Session::flash('success', 'Cập nhật thành công Danh Mục');
+        return true;
+
+    }
+
+    public function destroy($request)
+    {
+        //cũ không hiệu quả
+        $id = (int)$request->input('id');
+        $menu = Menu::where('id', $id)->first();
+        if ($menu) {
+            return Menu::where('id', $id)->orWhere('parent_id', $id)->delete();
+        }
+
+        return false;
+        //Tốt hơn
+//        try {
+//            //Xoá Root
+//
+//            Menu::where('id', $idMenu)->delete();
+//            //Lấy danh sách con của Root
+//            $listMenu = Menu::where('parent_id', $idMenu)->get();
+//            //Duyệt danh sách con
+//            foreach ($listMenu as $menu) {
+//                //Gọi đệ quy
+//                $this->Remove($menu->id);
+//                //Xoá danh sách con
+//                Menu::destroy($menu);
+//            }
+//            return true;
+//        } catch (Exception $e) {
+//            return false;
+//        }
     }
 }
