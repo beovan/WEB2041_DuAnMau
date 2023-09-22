6b641775c189b4ec3f072ac80f0587c8c2c61c78
beovan <truongvangiap04@gmail.com>
2023-09-22 17:19:30 +0700

    #1 add Upload complete

--- app/Http/Controllers/Admin/UploadController.php
+++ app/Http/Controllers/Admin/UploadController.php
@@ -16,6 +16,13 @@ class UploadController extends Controller
     }
     public function  store(Request $request)
     {
-    dd($request->file());
+        $url = $this->upload->store($request);
+        if ($url != false){
+            return response()->json([
+               'error' => false,
+               'url' => $url
+            ]);
+        }
+        return response()->json(['error' => true]);
     }
 }
