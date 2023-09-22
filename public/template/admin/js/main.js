81807116c26e7bdb8a1c1c781abb7fa5127e0aad
beovan <truongvangiap04@gmail.com>
2023-09-22 17:38:08 +0700

    #4 add ajax with url and thumb id

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -31,6 +31,7 @@ $('#upload').change(function (){
     const form = new FormData();
     form.append('file',$($this)[0].files[0]);
 
+
     $.ajax({
        processData: false,
        contentType:false,
