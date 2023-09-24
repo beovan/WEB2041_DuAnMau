bcf1a9e4c534e095a0d219a724f362ec2dc321f2
beovan <85023866+beovan@users.noreply.github.com>
2023-09-24 15:50:59 +0700

    fix ajax (#9)
    * Merge pull request #2 from beovan/main (#3)
    
    * Nothing
    
    * #4 add ajax with url and thumb id
    
    * Update Untitled Diagram.drawio
    
    * Update Untitled Diagram.drawio
    
    * Update Untitled Diagram.drawio
    
    * Update Untitled Diagram.drawio
    
    * fix ajax

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -29,7 +29,8 @@ function removeRow(id,url){
 /*Upload file */
 $('#upload').change(function (){
     const form = new FormData();
-    form.append('file',$($this)[0].files[0]);
+    form.append('file',$(this)[0].files[0]);
+
 
 
     $.ajax({
