6402ef3d30fd8fa87c9b30e32319924a07daf07c
beovan <truongvangiap04@gmail.com>
2023-09-24 18:02:47 +0700

    add ajax

--- public/template/admin/js/main.js
+++ public/template/admin/js/main.js
@@ -24,3 +24,24 @@ function removeRow(id,url){
         })
     }
 }
+
+
+/*Upload file */
+$('#upload').change(function (){
+    const form = new FormData();
+    form.append('file',$(this)[0].files[0]);
+
+
+
+    $.ajax({
+       processData: false,
+       contentType:false,
+       type: 'POST',
+        dateType:'JSON',
+        data : form,
+        url: '/admin/upload/services',
+        success: function (results){
+           console.log(results);
+        }
+    });
+});
