5b1fdb1ad717fd1d875db6fe3f8cd8b045837d64
beovan <truongvangiap04@gmail.com>
2023-09-30 10:27:12 +0700

    add side bar cart list #14

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -112,6 +112,25 @@
 
                             </ul>
                         </li>
+
+{{--Đơn hàng--}}
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-cart-plus"></i>
+                                <p> Giỏ Hàng
+                                    <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="/admin/customers" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Danh Sách Đơn Hàng</p>
+                                    </a>
+                                </li>
+
+                            </ul>
+                        </li>
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
